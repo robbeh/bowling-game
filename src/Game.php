6577657e54b26434b3fedd34d3c455f4b04f0b43
Rob .T <robbeh@github.com>
2014-08-22 22:05:22 +0100

    Add final tests & fully stable game app.
    Signed-off-by: Rob .T <robbeh@github.com>

--- src/Game.php
+++ src/Game.php
@@ -1,8 +1,79 @@
 <?php
+namespace Bowling;
+
 class Game
 {
+    private $rolls = array();
+    private $currentRoll = 0;
+    /**
+     * @param $pins
+     */
     public function roll($pins)
     {
+        $this->rolls[$this->currentRoll++] = $pins;
+    }
 
+    public function score()
+    {
+        $score = 0;
+        $firstInFrame = 0;
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isStrike($firstInFrame)) {
+                $score += 10 + $this->nextTwoBallsForStrike($firstInFrame);
+                $firstInFrame++;
+            } else if ($this->isSpare($firstInFrame)) {
+                $score += 10 + $this->nextBallForSpare($firstInFrame);
+                $firstInFrame += 2;
+            } else {
+                $score += $this->twoBallsInFrame($firstInFrame);
+                $firstInFrame += 2;
+            }
+        }
+        return $score;
+    }
+
+    /**
+     * @param $firstInFrame
+     * @return bool
+     */
+    public function isSpare($firstInFrame)
+    {
+        return $this->rolls[$firstInFrame] + $this->rolls[$firstInFrame + 1] == 10;
+    }
+
+    /**
+     * @param $firstInFrame
+     * @return bool
+     */
+    public function isStrike($firstInFrame)
+    {
+        return $this->rolls[$firstInFrame] == 10;
+    }
+
+    /**
+     * @param $firstInFrame
+     * @return mixed
+     */
+    public function nextTwoBallsForStrike($firstInFrame)
+    {
+        return $this->rolls[$firstInFrame + 1] + $this->rolls[$firstInFrame + 2];
+    }
+
+    /**
+     * @param $firstInFrame
+     * @return mixed
+     */
+    public function nextBallForSpare($firstInFrame)
+    {
+        return $this->rolls[$firstInFrame + 2];
+    }
+
+    /**
+     * @param $firstInFrame
+     * @return mixed
+     */
+    public function twoBallsInFrame($firstInFrame)
+    {
+        return $this->rolls[$firstInFrame] + $this->rolls[$firstInFrame + 1];
     }
 }
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -7,13 +7,64 @@ class BowlingTest extends PHPUnit_Framework_TestCase
 
     public function __construct()
     {
-        $this->g = new Game();
+        $this->g = new Bowling\Game();
     }
 
-    public function testCanRoll()
+    /**
+     * @param $n
+     * @param $pins
+     */
+    public function rollMany($n, $pins)
     {
-        $this->g->roll(0);
+        for ($i = 0; $i < $n; $i++) {
+            $this->g->roll($pins);
+        }
     }
 
+    public function rollSpare()
+    {
+        $this->g->roll(5);
+        $this->g->roll(5);
+    }
 
+    public function rollStrike()
+    {
+        $this->g->roll(10);
+    }
+
+    public function testGutterGame()
+    {
+        $this->rollMany(20, 0);
+        $this->assertEquals(0, $this->g->score());
+    }
+
+    public function testAllOnes()
+    {
+        $this->rollMany(20, 1);
+        $this->assertEquals(20, $this->g->score());
+    }
+
+    public function testOneSpare()
+    {
+       // $this->markTestIncomplete();
+        $this->rollSpare();
+        $this->g->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->g->score());
+    }
+
+    public function testOneStrike()
+    {
+        $this->rollStrike();
+        $this->g->roll(3);
+        $this->g->roll(4);
+        $this->rollMany(16, 0);
+        $this->assertEquals(24, $this->g->score());
+    }
+
+    public function perfectGame()
+    {
+        $this->rollMany(12, 10);
+        $this->assertEquals(300, $this->g->score());
+    }
 }
